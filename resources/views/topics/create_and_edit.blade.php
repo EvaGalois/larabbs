46e95214f0e5fdca191ff24a6c4bf167dcff64a1
vagrant <vagrant@homestead>
2020-12-06 11:36:24 +0000

    WYSIWYG 编辑器

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -4,7 +4,7 @@
 
   <div class="container">
     <div class="col-md-10 offset-md-1">
-      <div class="card ">
+      <div class="card">
 
         <div class="card-body">
           <h2 class="">
@@ -56,3 +56,22 @@
   </div>
 
 @endsection
+
+@section('styles')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
+
+@section('scripts')
+  <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+
+  <script>
+    $(document).ready(function() {
+      var editor = new Simditor({
+        textarea: $('#editor'),
+      });
+    });
+  </script>
+@stop
