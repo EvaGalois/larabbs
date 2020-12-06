dbf4eb5f988de9bd6061ed80a533b67d475bd0a9
vagrant <vagrant@homestead>
2020-12-06 06:49:47 +0000

    裁剪头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
-
 use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
@@ -25,7 +24,7 @@ class UsersController extends Controller
         $user->update($request->all());
 
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
