a940b4a8cb220bc4a22045fdff8081870c380a01
vagrant <vagrant@homestead>
2020-12-06 08:53:22 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,8 @@ function route_class()
     // 将当前请求的路由名称转换为 CSS 类名称，允许针对某个页面做页面样式定制
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
